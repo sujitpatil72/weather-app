fa52733dc7e48043e30f19fb5b9b5d6c5f15e0ef
sujit <sujit.patil72@hotmail.com>
2020-07-25 16:23:02 +0530

    added detect breakpoint service

---
+++ src/app/detect-breakpoints.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import {
+  BreakpointObserver,
+  BreakpointState,
+  Breakpoints,
+} from '@angular/cdk/layout';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DetectBreakpointsService {
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  isHandset(): Observable<boolean> {
+    return this.breakpointObserver
+      .observe(Breakpoints.Handset)
+      .pipe(map((result: BreakpointState) => result.matches));
+  }
+}
