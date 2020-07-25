229ef6ea18db0a72e1f96c0e3c2f5d50bd403c38
sujit <sujit.patil72@hotmail.com>
2020-07-25 19:11:54 +0530

    imported HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
@@ -14,6 +14,7 @@ import { WeatherDetailsDialog } from './dialogs/weather-details-dialog/weather-d
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MaterialModule,
     ReactiveFormsModule,
   ],
