aa5078b92af64dfbe479591df6d4055f9bb63fff
sujit <sujit.patil72@hotmail.com>
2020-07-25 16:08:36 +0530

    added WeatherDetailsDialog html and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
+import { WeatherDetailsDialog } from './dialogs/weather-details-dialog/weather-details-dialog';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, WeatherDetailsDialog],
   imports: [
     BrowserModule,
     AppRoutingModule,
---
+++ src/app/dialogs/weather-details-dialog/weather-details-dialog.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'weather-details-dialog',
+  templateUrl: './weather-details-dialog.html',
+  styleUrls: ['./weather-details-dialog.scss'],
+})
+export class WeatherDetailsDialog {}
